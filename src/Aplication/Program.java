c2178db6fa6cd65e7d3e3ca808ce1ebe89010921
MessiasFilho <88682777+MessiasFilho@users.noreply.github.com>
2022-06-01 16:53:12 -0300

    Initial commit

---
+++ src/Aplication/Program.java
@@ -0,0 +1,23 @@
+package Aplication; // diretorio  que tem com pricipal reponsabilidade separar as classes 
+
+public class Program { // uma classe util para representar objetos, detro dela é 
+						// comum declarar atibutos e metodos 
+
+	public static void main(String[] args) // metodo principal determina o ponto de inicio  de qulquer aplicação java 
+	{
+		// TODO Auto-generated method stub
+		
+	// System e uma classe com um fluxo de entrada padrao e saida padrao e saida de erro acesso a
+	// propriedades e variáveis ​​de ambiente definidas externamente; um meio de carregar arquivos e
+	// bibliotecas; e um método utilitário para copiar rapidamente uma parte de uma matriz.
+	
+	// OUT fluxo que correspode a saida de exibição ou destino de saida especificado pelo anbiente host ou usuario 	
+	
+	// println é um metodo gera uma string de texto, cria uma nova linha abaixo da atual e então posiciona o cursor nesta linha.
+	// () parametro que pode possuir ou nao atributos ou metodos 
+		
+		System.out.println(" hello Word  !!");
+
+	}
+
+}
